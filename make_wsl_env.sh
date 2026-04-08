b09ed05309c27b2cb8c71ec7079b4b4fef3a178d
Reaper <reaperof7@gmail.com>
2026-04-08 12:18:46 +0530

    Junk Clearing md

--- make_wsl_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Strip Windows \r line endings and replace localhost with WSL host IP
-sed 's/\r//; s/localhost/10.255.255.254/' /mnt/c/Users/Adity/Desktop/Projects/DBMS\ LSTM/sovereign-analytics/.env > ~/wsl.env
-echo "[+] wsl.env created at ~/wsl.env:"
-head -2 ~/wsl.env
